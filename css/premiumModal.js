f9e254d09095624f9d92f6f74b995440ba9c7d57
Virajp4 <viraj.patel482@gmail.com>
2023-09-09 15:24:36 +0530

    updated head and about

--- css/premiumModal.js
+++ css/premiumModal.js
@@ -1,8 +1,8 @@
-var widthDesktop =  window.innerWidth;
+var widthDesktop = window.innerWidth;
 var mainModal = document.getElementById('mainModal');
 
-if(widthDesktop>520){
+if (widthDesktop > 520) {
   $("#mainModal").css({
-      "margin-left": "-30vw";
+    "margin-left": "-30vw";
   });
 }
